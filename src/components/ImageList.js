63cad09779f88945b9eb64413fc2311544dee65d
github username <github email address>
2019-05-19 16:20:21 -0700

    ImageCard.js component to manage the layout of images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
-  const images = props.images.map((image) => {
-    return <img key={image.id} src={image.urls.regular} alt={image.description}/>
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 }
 
 export default ImageList;
