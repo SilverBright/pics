982a1c7dc7f9b665440ef40602fe7fbf78dba0e4
github username <github email address>
2019-05-19 11:05:00 -0700

    refactor form to be a controlled form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: ''};
+
+  // onInputChange(event) {
+  //   console.log(event.target.value)
+  // }
+
   render () {
     return (
-    <div className="ui segment">
-      <form className="ui form">
-        <div className="field">
-          <label>Image Search</label>
-        <input type="text"/>
-        </div>
-      </form>
-    </div>
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input 
+              type="text" 
+              value={this.state.term} 
+              onChange={e => this.setState({ term: e.target.value })} 
+            />
+          </div>
+        </form>
+      </div>
     );
   }
 }
